cdd09febc33114667f41a0cc52fe4d073386ebd9
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-12-10 22:55:42 -0500

    Initial Commit

---
+++ TaylorSwiftAlbums/TaylorSwiftAlbumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaylorSwiftAlbumsApp.swift
+//  TaylorSwiftAlbums
+//
+//  Created by Skye Willow Harris-Stoertz on 2024-12-10.
+//
+
+import SwiftUI
+
+@main
+struct TaylorSwiftAlbumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
